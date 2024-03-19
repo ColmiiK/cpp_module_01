0c82988654509d7b1132e88b74787c1e648bd99f
alvega-g <alvarvg@gmail.com>
2024-03-19 16:15:45 +0100

    refactor, started

---
+++ ex04/include/main.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 15:34:13 by alvega-g          #+#    #+#             */
+/*   Updated: 2024/03/19 15:35:28 by alvega-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int printError(std::string msg);
+
+#endif
--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,24 +6,20 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 12:33:23 by alvega-g          #+#    #+#             */
-/*   Updated: 2024/03/19 12:56:21 by alvega-g         ###   ########.fr       */
+/*   Updated: 2024/03/19 16:15:02 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
-#include <iostream>
-#include <fstream>
-
-int printError(std::string msg){
-	std::cerr << msg << std::endl;
-	return (1);
-}
+#include <main.hpp>
 
 int main(int ac, char **av) {
 	if (ac != 4){
 		return (printError("Incorrect number of arguments"));
 	}
 	std::ifstream file(av[1]);
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	std::string replaced;
 	std::string line;
 	std::string text;
 	
@@ -35,6 +31,18 @@ int main(int ac, char **av) {
 	}
 	else
 		return (printError("Unable to open file"));
+	int start = text.find(s1);
+	int len = s1.length();
+	replaced.copy(text, text.length(), 0);
+	while (start != -1) {
+		replaced.erase(start, len);
+		for (int i = 0; i < s2.length(); i++){
+			replaced.insert(start, s2[i]);
+		}
+	}
 
+	std::cout << replaced << std::endl;
+	
+	
 
 }
