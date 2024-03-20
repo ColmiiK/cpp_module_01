30b01ae24eab7caeae4433c4094141aff1126a1d
alvega-g <alvarg@gmail.com>
2024-03-20 16:14:40 +0100

    ex05 done

--- ex05/include/Harl.hpp
+++ ex05/include/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 13:11:31 by alvega-g          #+#    #+#             */
-/*   Updated: 2024/03/20 13:25:53 by alvega-g         ###   ########.fr       */
+/*   Updated: 2024/03/20 16:14:09 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 
 # include <string>
 # include <iostream>
+# include <map>
 
 class Harl {
 	private:
 		void debug( void );
 		void info( void );
 		void warning( void );
-		void error( void );	
+		void error( void );
+		typedef void (Harl::*ptrFunc)( void );
+		std::map<std::string, ptrFunc> mapFunc;
 	public:
+		Harl( void );
+		~Harl( void );
 		void complain( std::string level );
-}
+};
 
 #endif
