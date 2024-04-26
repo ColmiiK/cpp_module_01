080d0391a5900c99664860890bf405ac1017fe9f
alvega-g <alvarg@gmail.com>
2024-04-26 17:43:04 +0200

    fixed weapon segfault

--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:53:12 by alvega-g          #+#    #+#             */
-/*   Updated: 2024/03/19 12:26:56 by alvega-g         ###   ########.fr       */
+/*   Updated: 2024/04/26 17:42:14 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 HumanB::HumanB(std::string name) : name(name){
 	std::cout << "HumanB " << name << " created with ";
 	std::cout << "no weapon" << std::endl;
+	weapon = NULL;
 }
 
 HumanB::~HumanB(void) {
@@ -27,5 +28,8 @@ void HumanB::setWeapon(Weapon &weapon) {
 
 void HumanB::attack(void) {
 	std::cout << name << " attacks with their ";
-	std::cout << HumanB::weapon->getType() << std::endl;
+	if (this->weapon == nullptr)
+		return ;
+	else
+		std::cout << HumanB::weapon->getType() << std::endl;
 }
